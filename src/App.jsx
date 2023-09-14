2dd85ae6b757cf7a1a8e6154757a269d846c9498
MatiPino <mati.pinos@duocuc.cl>
2023-09-14 14:33:11 -0300

    Registro cambiado a primereact

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import "primereact/resources/themes/lara-light-indigo/theme.css";
-// import "primereact/resources/primereact.min.css";
-// import { PrimeReactProvider } from 'primereact/context'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import { PrimeReactProvider } from 'primereact/api';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MenuLateral from '../components/Menubar';
 
@@ -14,7 +14,7 @@ import { Principal } from '../components/Principal';
 
 function App() {
   return (
-    // <PrimeReactProvider>
+    <PrimeReactProvider>
     <Router>
       <Routes>
         <Route path="/" element={<MenuLateral/>}/>
@@ -22,7 +22,7 @@ function App() {
         <Route path="/Registrar" element={<RegistrarUsuarios/>} />
       </Routes>
     </Router>
-    // </PrimeReactProvider>
+    </PrimeReactProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// import { PrimeReactProvider } from 'primereact/context'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { PrimeReactProvider } from 'primereact/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <PrimeReactProvider>
-        <App/>
-    /* </PrimeReactProvider> */
-)
+    <PrimeReactProvider>
+        <App />
+    </PrimeReactProvider>
+);
+
