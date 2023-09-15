37ca17700da14d09af7056c24c5d0c8f0266c680
MatiPino <mati.pinos@duocuc.cl>
2023-09-15 04:40:10 -0300

    menulateral

--- components/IniciarSesion.jsx
+++ components/IniciarSesion.jsx
@@ -1,24 +1,51 @@
 import React, { useState } from 'react';
-import { Chips } from 'primereact/chips';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
 import { Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
 
 export const IniciarSesion = () => {
-  const [usuario, setUsuario] = useState([]); // Estado para el campo de usuario
-  const [contraseña, setContraseña] = useState([]); // Estado para el campo de contraseña
+  const [cargando, setCargando] = useState(false);
+
+  const estructuraInicio = {
+    usuario:'',
+    contrasena:''
+  }
+
+  const [formData, setFormData] = useState(estructuraInicio)
+  const handleChange = ({target}) => {
+    const {name, value} = target
+    setFormData({...formData,[name]:value})
+  }
+
+  const cargar = () => {
+    setCargando(true);
+    
+    setTimeout(() => {
+      setCargando(false);
+      submit(); // Llamar a la función submit cuando cargando sea falso
+    }, 2000);
+  };
+  
+  const submit = () => {
+    console.log(formData);
+  };
 
   return (
     <div className="p-card" style={{ background: 'white', padding: '20px' }}>
       <h2>Iniciar Sesion</h2>
-      <span className="p-float-label" style={{ margin: '20px' }}>
-        <Chips id="usuario" value={usuario} onChange={(e) => setUsuario(e.value)} />
-        <label htmlFor="usuario">Usuario</label>
-      </span>
-      <span className="p-float-label" style={{ margin: '20px' }}>
-        <Chips id="contraseña" value={contraseña} onChange={(e) => setContraseña(e.value)} />
-        <label htmlFor="contraseña">Contraseña</label>
-      </span>
-      <div style={{ marginBottom: '20px' }}>
-        <button style={{ backgroundColor: 'green'}}>Iniciar sesion</button>
+      <div style={{padding:'10px'}}>
+        <span className="p-float-label">
+          <InputText value={formData.usuario} name='usuario' onChange={handleChange} />
+          <label htmlFor="usuario">Usuario</label>
+        </span>
+        <span className="p-float-label" style={{marginTop:'25px'}}>
+        <Password value={formData.contrasena} name='contrasena' onChange={handleChange} feedback={false} />
+        <label style={{textAlign:'center'}} htmlFor="password">Contraseña</label>
+        </span>
+      </div>
+      <div className="card flex justify-content-center">
+        <Button style={{margin:'auto'}} label='Iniciar Sesion' severity='success' loading={cargando} onClick={cargar}/>
       </div>
       <p style={{ fontSize: '10px' }}>
         ¿Tienes problemas para iniciar sesión, presiona <Link to="/" style={{ color: 'blue' }}>aquí</Link>?
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { PrimeReactProvider } from 'primereact/api';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MenuLateral from '../components/Menubar';
 
+<<<<<<< Updated upstream
 import { IniciarSesion } from '../components/IniciarSesion'
 import { RegistrarUsuarios } from '../components/RegistrarUsuarios'
 import { Principal } from '../components/Principal';
+=======
+import { IniciarSesion } from './components/IniciarSesion'
+import { RegistrarUsuarios } from './components/RegistrarUsuarios'
+>>>>>>> Stashed changes
 
 function App() {
+  const [menuVisible, setMenuVisible] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <PrimeReactProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<MenuLateral/>}/>
-        <Route path="/Iniciar-sesion" element={<IniciarSesion/>} />
-        <Route path="/Registrar" element={<RegistrarUsuarios/>} />
-      </Routes>
-    </Router>
+      <Router>
+          <div className={`MenuCard ${menuVisible ? '' : 'menu-hidden'}`}>
+            <MenuLateral menuVisible={menuVisible} toggleMenu={toggleMenu} />
+            <Routes>
+              <Route path="/"/>
+              <Route path="/Iniciar-sesion" element={<IniciarSesion />} />
+              <Route path="/Registrar" element={<RegistrarUsuarios />} />
+            </Routes>
+          </div>
+      </Router>
     </PrimeReactProvider>
   );
 }
 
 export default App;
+
