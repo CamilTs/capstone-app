d9fa706dd1f2ec2bfee4e2a009be565d99d9d3cc
CamilTs <camil.tapias@duocuc.cl>
2023-09-25 23:26:32 -0300

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/capstone-app/",
+  base: "https://CamilTs.github.io/capstone-app/",
   plugins: [react()],
 });
