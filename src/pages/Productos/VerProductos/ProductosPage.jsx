12f7dc24e85c5c4d86953cdf2a2e81cde63e9b39
MatiPino <mati.pinos@duocuc.cl>
2023-10-06 23:58:57 -0300

    DataTable modificada
    La tabla producto fue modificada, ahora puede descargar en excel con export de primereact, puede exportar em PDF, nos muestra que productos tienen bajo o algo stock, nos permite editar e eliminar productos mas ordenado, permite visualizar entre 5,10,15 productos a la vez, además se transformo en un scroll en caso de requerirse mas pequeña la tabla.

--- src/pages/Productos/VerProductos/ProductosPage.jsx
+++ src/pages/Productos/VerProductos/ProductosPage.jsx
@@ -1,5 +1,4 @@
-import * as XLSX from "xlsx";
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
@@ -12,6 +11,8 @@ import { formatoCurrencyCLP } from "../../../components/FormatoDinero";
 import "../../../CSS/Productos.css";
 import { InputText } from "primereact/inputtext";
 import { Dialog } from "primereact/dialog";
+import { classNames } from "primereact/utils";
+import { InputNumber } from "primereact/inputnumber";
 
 export const Productos = () => {
   const { user } = useAuth();
@@ -23,6 +24,7 @@ export const Productos = () => {
   const [buscarText, setBuscarText] = useState("");
   const [productosFiltrados, setProductosFiltrados] = useState([]);
   const productosCliente = user ? productos.filter((el) => el.clienteId == user.id) : [];
+  const [submitted, setSubmitted] = useState(false);
 
   const toast = useRef(null);
   const mostrar = () => {
@@ -80,11 +82,41 @@ export const Productos = () => {
     handleBuscar(nuevoTexto);
   };
 
+  const cantidadProductos = (rowData) => {
+    const cantidadClase = classNames("custom-circle", {
+      "bg-red-100 text-red-900": rowData.cantidad > 0 && rowData.cantidad < 8,
+      "bg-yellow-100 text-yellow-900": rowData.cantidad > 8 && rowData.cantidad < 15,
+      "bg-green-100 text-green-900": rowData.cantidad > 14,
+    });
+    return <div className={cantidadClase}>{rowData.cantidad}</div>;
+  };
+
+  // Into PrimeReact para exportar archivos //
+
+  const cols = [
+    { field: "codigoBarra", header: "Código de barra" },
+    { field: "producto", header: "Productos" },
+    { field: "categoria", header: "Categorias" },
+    { field: "cantidad", header: "Cantidad" },
+    { field: "fecha", header: "Fecha" },
+    { field: "precio", header: "Precio" },
+  ];
+
+  const exportarColumnas = cols.map((col) => ({ title: col.header, dataKey: col.field }));
+
+  const exportarPdf = () => {
+    import("jspdf").then((jsPDF) => {
+      import("jspdf-autotable").then(() => {
+        const doc = new jsPDF.default(0, 0);
+        doc.autoTable(exportarColumnas, productosFiltrados.length > 0 ? productosFiltrados : productosCliente);
+        doc.save("productos.pdf");
+      });
+    });
+  };
+
   const exportarExcel = () => {
     const datosExportar = productosFiltrados.length > 0 ? productosFiltrados : productosCliente;
-
-    // Crear matiz de objetos que represente los datos de la tabla
-    const datos = datosExportar.map((producto) => ({
+    const datosExcel = datosExportar.map((producto) => ({
       Codigo_de_barra: producto.codigoBarra,
       Producto: producto.producto,
       Categoria: producto.categoria,
@@ -93,96 +125,45 @@ export const Productos = () => {
       Precio: formatoCurrencyCLP(producto.precio),
     }));
 
-    // Crear una hoja de calculo excel
-    const ws = XLSX.utils.json_to_sheet(datos);
-
-    // Crear un libro de trabajo y adjuntar la hoja de calculo en el libro
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "Productos");
-
-    // Generar el archivo excel en formato blob
-    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-
-    // Convertir el arraybuffer en blob y poder descargarlo
-    const blob = new Blob([excelBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
-
-    // Crear un objeto URL para el blob y crear un enlace de descarga del archivo
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "productos.xlsx";
-    document.body.appendChild(a);
-    a.click();
-
-    // Liberar el objeto URL blob
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
+    import("xlsx").then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(datosExcel);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer = xlsx.write(workbook, { bookType: "xlsx", type: "array" });
+      saveAsExcelFile(excelBuffer, "productos");
+    });
   };
 
-  const acciones = (rowData) => {
+  const saveAsExcelFile = (buffer, fileName) => {
+    import("file-saver").then((module) => {
+      if (module && module.default) {
+        let EXCEL_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+        let EXCEL_EXTENSION = ".xlsx";
+        const data = new Blob([buffer], { type: EXCEL_TYPE });
+        module.default.saveAs(data, fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION);
+      }
+    });
+  };
+
+  const header = (
+    <div className="exportarOption">
+      <Button label="Excel" icon="pi pi-file-excel" severity="success" rounded onClick={exportarExcel} data-pr-tooltip="XLS" />
+      <Button label="PDF" icon="pi pi-file-pdf" severity="danger" rounded onClick={exportarPdf} data-pr-tooltip="PDF" />
+    </div>
+  );
+
+  const productDialogFooter = (
+    <React.Fragment>
+      <Button label="Guardar" icon="pi pi-check" severity="success" onClick={guardarCambios} />
+      <Button label="Cancelar" icon="pi pi-times" severity="danger" onClick={() => setFormularioVisible(false)} />
+    </React.Fragment>
+  );
+
+  const actionBodyTemplate = (rowData) => {
     return (
-      <div>
-        <Button className="p-button p-button-info p-button-icon-only" onClick={() => abrirFormularioEdicion(rowData)}>
-          <span className="pi pi-pencil" />
-        </Button>
-        <Dialog header="Editar Producto" visible={formularioVisible} onHide={() => setFormularioVisible(false)}>
-          <div className="formulario-edicion">
-            <div className="p-field">
-              <label htmlFor="codigoBarra">Codigo de barra: </label>
-              <InputText
-                value={productoAModificar.codigoBarra}
-                onChange={(e) => setProductoAModificar({ ...productoAModificar, codigoBarra: e.target.value })}
-              />
-            </div>
-            <div className="p-field">
-              <label htmlFor="producto">Nombre: </label>
-              <InputText
-                value={productoAModificar.producto}
-                onChange={(e) => setProductoAModificar({ ...productoAModificar, producto: e.target.value })}
-              />
-            </div>
-            <div className="p-field">
-              <label htmlFor="categoria">Categoría: </label>
-              <InputText
-                value={productoAModificar.categoria}
-                onChange={(e) => setProductoAModificar({ ...productoAModificar, categoria: e.target.value })}
-              />
-            </div>
-            <div className="p-field">
-              <label htmlFor="cantidad">Cantidad: </label>
-              <InputText
-                value={productoAModificar.cantidad}
-                onChange={(e) => setProductoAModificar({ ...productoAModificar, cantidad: e.target.value })}
-              />
-            </div>
-            <div className="p-field">
-              <label htmlFor="precio">Precio: </label>
-              <InputText
-                value={productoAModificar.precio}
-                onChange={(e) => setProductoAModificar({ ...productoAModificar, precio: e.target.value })}
-              />
-            </div>
-            <Button label="Guardar Cambios" onClick={guardarCambios} />
-          </div>
-        </Dialog>
-        <Button icon="pi pi-times" rounded severity="danger" aria-label="Cancel" onClick={() => handleEliminarProducto(rowData.id)} />
-        <ConfirmDialog
-          visible={confirmDialogVisible}
-          onHide={() => setConfirmDialogVisible(false)}
-          message="¿Seguro que deseas eliminar este producto?"
-          header="Confirmar Eliminación"
-          icon="pi pi-exclamation-triangle"
-          acceptClassName="p-button-danger"
-          acceptLabel="Sí"
-          rejectLabel="No"
-          footer={
-            <div>
-              <Button label="Eliminar" icon="pi pi-trash" className="p-button-danger" onClick={handleEliminarProductoClick} />
-              <Button label="Cancelar" icon="pi pi-times" className="p-button-text" onClick={() => setConfirmDialogVisible(false)} />
-            </div>
-          }
-        />
-      </div>
+      <React.Fragment>
+        <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={() => abrirFormularioEdicion(rowData)} />
+        <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => handleEliminarProducto(rowData.id)} />
+      </React.Fragment>
     );
   };
 
@@ -203,20 +184,107 @@ export const Productos = () => {
             </Button>
           </div>
         </div>
-        <div className="contenidoTabla">
-          <DataTable showGridlines value={productosFiltrados.length > 0 ? productosFiltrados : productosCliente}>
+        <div>
+          <DataTable
+            showGridlines
+            header={header}
+            value={productosFiltrados.length > 0 ? productosFiltrados : productosCliente}
+            paginator
+            rows={10}
+            rowsPerPageOptions={[5, 10, 15]}
+            scrollable
+            scrollHeight="480px"
+            style={{ minWidth: "40rem" }}
+          >
             <Column field="codigoBarra" header="Código de barra" body={(rowData) => rowData.codigoBarra} />
             <Column field="producto" header="Productos" body={(rowData) => rowData.producto} />
             <Column field="categoria" header="Categorias" body={(rowData) => rowData.categoria} />
-            <Column field="cantidad" header="Cantidad" body={(rowData) => rowData.cantidad} />
+            <Column field="cantidad" header="Cantidad" body={cantidadProductos} />
             <Column field="fecha" header="Fecha" body={(rowData) => (rowData.fecha instanceof Date ? rowData.fecha.toLocaleDateString() : "")} />
             <Column field="precio" header="Precio" body={(rowData) => formatoCurrencyCLP(rowData.precio)} />
-            <Column header="Acciones" body={acciones} />
+            <Column header="Acciones" body={actionBodyTemplate} exportable={false} style={{ minWidth: "12rem" }} />
           </DataTable>
-          <div>
-            <Button className="p-button-raised p-button-success" label="Exportar a Excel" icon="pi pi-file-excel" onClick={exportarExcel} />
-          </div>
         </div>
+        <Dialog
+          header="Editar Producto"
+          style={{ width: "32rem" }}
+          breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+          className="p-fluid"
+          visible={formularioVisible}
+          footer={productDialogFooter}
+          onHide={setFormularioVisible}
+        >
+          <div className="field">
+            <label htmlFor="codigoBarra" className="font-bold">
+              Codigo de barra
+            </label>
+            <InputText
+              value={productoAModificar.codigoBarra}
+              onChange={(e) => setProductoAModificar({ ...productoAModificar, codigoBarra: e.target.value })}
+              required
+              autoFocus
+              className={classNames({ "p-invalid": submitted && !productoAModificar.codigoBarra })}
+            />
+            {submitted && !productoAModificar.codigoBarra && <small className="p-error">Codigo de barra es requerido.</small>}
+          </div>
+          <div className="field">
+            <label htmlFor="producto" className="font-bold">
+              Nombre
+            </label>
+            <InputText
+              value={productoAModificar.producto}
+              onChange={(e) => setProductoAModificar({ ...productoAModificar, producto: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="categoria" className="font-bold">
+              Categoría
+            </label>
+            <InputText
+              value={productoAModificar.categoria}
+              onChange={(e) => setProductoAModificar({ ...productoAModificar, categoria: e.target.value })}
+            />
+          </div>
+          <div className="formgrid grid">
+            <div className="field col">
+              <label htmlFor="cantidad" className="font-bold">
+                Cantidad
+              </label>
+              <InputNumber
+                id="cantidad"
+                value={productoAModificar.cantidad}
+                onChange={(e) => setProductoAModificar({ ...productoAModificar, cantidad: e.target.value })}
+              />
+            </div>
+            <div className="field col">
+              <label htmlFor="precio" className="font-bold">
+                Precio
+              </label>
+              <InputNumber
+                id="precio"
+                value={productoAModificar.precio}
+                onChange={(e) => setProductoAModificar({ ...productoAModificar, precio: e.target.value })}
+              />
+            </div>
+          </div>
+        </Dialog>
+
+        <ConfirmDialog
+          visible={confirmDialogVisible}
+          onHide={() => setConfirmDialogVisible(false)}
+          message="¿Seguro que deseas eliminar este producto?"
+          header="Confirmar Eliminación"
+          icon="pi pi-exclamation-triangle"
+          acceptClassName="p-button-danger"
+          acceptLabel="Sí"
+          rejectLabel="No"
+          footer={
+            <div>
+              <Button label="Eliminar" icon="pi pi-trash" className="p-button-danger" onClick={handleEliminarProductoClick} />
+              <Button label="Cancelar" icon="pi pi-times" className="p-button-text" onClick={() => setConfirmDialogVisible(false)} />
+            </div>
+          }
+        />
       </div>
     </div>
   );
