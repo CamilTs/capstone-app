8be88c6a57e89bc91f4ce91e03dc66cc27001725
MatiPino <mati.pinos@duocuc.cl>
2023-10-10 02:27:07 -0300

    Styled iniciar sesion
    Estilos para inicar sesion

--- src/pages/Cuentas/IniciarSesion/IniciarSesionPage.jsx
+++ src/pages/Cuentas/IniciarSesion/IniciarSesionPage.jsx
@@ -13,14 +13,57 @@ import styled from "styled-components";
 import { api, useApi } from "../../../api/api";
 // import { autenticado, revisandoAutentication } from "../../../store/auth";
 
+const Contenedor = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 60px);
+`;
+
 const Titulo = styled.h2`
   text-transform: uppercase;
-  margin: 2rem 0 2rem 0;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-shadow: 0 1px 0 #538a90, 0 2px 0 #538a70, 0 3px 0 #538a75, 0 4px 0 #538a80, 0 5px 0 #538a90, 0 6px 1px rgba(0, 0, 0, 0.1),
+    0 0 5px rgba(0, 0, 0, 0.5), 0 1px 10px rgba(0, 0, 0, 0.1), 0 3px 5px rgba(0, 0, 0, 0.7), 0 5px 10px rgba(0, 0, 0, 0.5),
+    0 10px 10px rgba(0, 0, 0, 0.3), 0 10px 20px rgba(0, 0, 0, 0.15);
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: white;
+  padding: 2rem;
+  border-radius: 1rem;
+  gap: 1rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.65);
+`;
+
+const ContenedorCampos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+const ContenedorSpan = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  border-radius: 0.5rem;
 `;
 
 export const IniciarSesionPage = () => {
   const { login } = useAuth();
-  const { post, loading, get } = useApi();
+  const { post, loading } = useApi();
   const navigate = useNavigate();
 
   // const dispatch = useDispatch();
@@ -63,25 +106,25 @@ export const IniciarSesionPage = () => {
   // };
 
   return (
-    <Content width="auto" margin="auto">
-      <form onSubmit={submit}>
+    <Contenedor>
+      <Form onSubmit={submit}>
         <Titulo>Iniciar Sesión</Titulo>
-        <div style={{ display: "grid", placeContent: "center" }}>
-          <span className="p-float-label">
+        <ContenedorCampos>
+          <ContenedorSpan className="p-float-label p-input-icon-left">
+            <i className="pi pi-user"></i>
             <InputText value={formData.rut} name="rut" onChange={handleChange} />
             <label htmlFor="rut">Rut</label>
-          </span>
-          <span className="p-float-label" style={{ marginTop: "20px", marginBottom: "20px" }}>
-            <Password value={formData.contrasena} name="contrasena" onChange={handleChange} feedback={false} />
-            <label style={{ textAlign: "center" }} htmlFor="password">
-              Contraseña
-            </label>
-          </span>
+          </ContenedorSpan>
+          <ContenedorSpan className="p-float-label p-input-icon-left">
+            <i className="pi pi-eye"></i>
+            <InputText type="password" value={formData.contrasena} name="contrasena" onChange={handleChange} feedback={false} />
+            <label htmlFor="password">Contraseña</label>
+          </ContenedorSpan>
+        </ContenedorCampos>
+        <div>
+          <Button label="Iniciar Sesión" disabled={loading} severity="success" rounded onClick={submit} />
         </div>
-        <div className="card flex justify-content-center">
-          <Button style={{ margin: "auto" }} label="Iniciar Sesión" disabled={loading} severity="success" onClick={submit} />
-        </div>
-      </form>
-    </Content>
+      </Form>
+    </Contenedor>
   );
 };
